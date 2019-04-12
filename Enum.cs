8c662061c93b6d0a591cc88a5e5251f2a222e3b2
TroyMateo <48836188+TroyMateo@users.noreply.github.com>
2019-04-12 09:16:13 -0700

    Example 09_Edit

--- Enum.cs
+++ Enum.cs
@@ -1,14 +1,18 @@
-﻿/*https://www.dotnetperls.com/enum
+/*https://www.dotnetperls.com/enum
  * Author: Sam Allen
  * Date: 04/12/2019
  * Edited By: Troy Mateo
  * This is a c# program demonstrating the use of enums
+ * 
+ * 1) I added the value2 and value3 variables and printed them out at the end to demonstrate the 
+ * the underlying integer value for enums
  */
 
 using System;
 
 class Program
 {
+    // This is the list of enum variables created all with their inherit integer values.
     enum Importance
     {
         None,
@@ -32,5 +36,14 @@ class Program
         {
             Console.WriteLine("True");
         }
+
+        int value2 = (int)Importance.Regular;
+        int value3 = (int)Importance.Critical;
+
+        Console.WriteLine(value2);
+        Console.WriteLine(value3);
+
     }
 }
+
+//Output would be true, 2, 4.
