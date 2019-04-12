cabc45ed89310196978a4aa6624ac517089f7fe8
TroyMateo <48836188+TroyMateo@users.noreply.github.com>
2019-04-11 22:38:57 -0700

    Example 06

---
+++ Concatente.cs
@@ -0,0 +1,18 @@
+﻿/*
+ * https://www.sanfoundry.com/csharp-program-concatenate-strings/
+ * Author: Manish Bhojasia
+ * C# Program to Concatenate Two Strings
+ * Edited by: Troy Mateo
+ */
+using System;
+class Program
+{
+    static void Main()
+    {
+        string s1 = "Good";
+        string s2 = "Morning";
+        string s3 = string.Concat(s1, s2);
+        Console.WriteLine(s3);
+        Console.ReadLine();
+    }
+}
